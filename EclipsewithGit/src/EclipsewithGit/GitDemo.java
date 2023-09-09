0129d59031b6715966beadd487de796af424cbd6
madagalakumar <jagadeeshmm6143@gmail.com>
2023-09-10 00:18:35 +0530

    java code is created using eclipse

---
+++ EclipsewithGit/src/EclipsewithGit/GitDemo.java
@@ -0,0 +1,10 @@
+package EclipsewithGit;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am using git in eclipse");
+	}
+
+}
